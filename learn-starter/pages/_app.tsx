7dd0a123d9597dcde688f635a868c2c1590dff4b
Андрей Мерлов <merlov@neolant.ru>
2020-07-22 17:36:30 +0300

    добавил typescript

--- learn-starter/pages/_app.tsx
+++ learn-starter/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { UIDReset } from 'react-uid';
+import { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
     return <UIDReset>
         <Component {...pageProps} />
     </UIDReset>
